faa2732f67b4e3cc824bbef160c837f9f213b107
hejia <1464946535@qq.com>
2017-07-23 16:21:29 +0800

    up

---
+++ Assets/Script/SystemMonitor.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class SystemMonitor : MonoBehaviour
+{
+    public bool showFps = true;
+
+    public float f_UpdateInterval = 0.5F;
+
+    private float f_LastInterval;
+
+    private int i_Frames = 0;
+
+    private float f_Fps;
+
+    void Start()
+    {
+        if (!showFps)
+        {
+            Destroy(this);
+        }
+
+        //Application.targetFrameRate=60;
+        f_LastInterval = Time.realtimeSinceStartup;
+
+        i_Frames = 0;
+    }
+
+    void Update()
+    {
+        if (!showFps)
+            return;
+
+        ++i_Frames;
+
+        if (Time.realtimeSinceStartup > f_LastInterval + f_UpdateInterval)
+        {
+            f_Fps = i_Frames / (Time.realtimeSinceStartup - f_LastInterval);
+
+            i_Frames = 0;
+
+            f_LastInterval = Time.realtimeSinceStartup;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (!showFps)
+            return;
+
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 20;
+        style.normal.textColor = new Color(1, 1, 1);
+
+        string text = "";
+        text += "总内存：" + ByteToM(Profiler.GetTotalAllocatedMemoryLong()).ToString("F") + "M" + "\n";
+        text += "堆内存：" + ByteToM(Profiler.GetMonoUsedSizeLong()).ToString("F") + "M" + "\n";
+        text += "FPS: " + f_Fps.ToString("f2") + "\n";
+
+        GUI.Label(new Rect(5, Screen.height - 75, 200, 200), text, style);
+    }
+
+    static float ByteToM(long byteCount)
+    {
+        return (float)(byteCount / (1024.0f * 1024.0f));
+    }
+}
---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image mBgImg;
+    private Image mJoystickImg;
+    private Vector3 mInputVector;
+
+    void Start()
+    {
+        Text msgBox = transform.Find("MessageBox").GetComponent<Text>();
+        Image bgImg = transform.Find("JoystickBg").GetComponent<Image>();
+        Image joystickImg = bgImg.transform.Find("joystick").GetComponent<Image>();
+
+        mBgImg = bgImg;
+        mJoystickImg = joystickImg;
+
+        //bgImg.gameObject.SetActive(false);
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(mBgImg.rectTransform,
+            eventData.position, eventData.pressEventCamera, out pos))
+        {
+            pos.x = pos.x / mBgImg.rectTransform.sizeDelta.x;
+            pos.y = pos.y / mBgImg.rectTransform.sizeDelta.y;
+
+            //Debug.Log("Hey");
+            mInputVector = new Vector3(pos.x * 2 + 1, 0, pos.y * 2 - 1);
+            mInputVector = (mInputVector.magnitude > 1.0f) ? mInputVector.normalized : mInputVector;
+
+            mJoystickImg.rectTransform.anchoredPosition =
+                new Vector3(mInputVector.x * (mBgImg.rectTransform.sizeDelta.x / 3)
+                , mInputVector.z * (mBgImg.rectTransform.sizeDelta.y / 3));
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        mInputVector = Vector3.zero;
+        mJoystickImg.rectTransform.anchoredPosition = Vector3.zero;
+    }
+
+    public float Horizontal()
+    {
+        if (mInputVector.x != 0)
+        {
+            return mInputVector.x;
+        }
+        else
+        {
+            return Input.GetAxis("Horizontal");
+        }
+    }
+
+    public float Vertical()
+    {
+        if (mInputVector.z != 0)
+        {
+            return mInputVector.z;
+        }
+        else
+        {
+            return Input.GetAxis("Vertical");
+        }
+    }
+}
