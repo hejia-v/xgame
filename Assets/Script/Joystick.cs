ffc42efc57a0ecb5ce0abd1b9be11fb1812ddae0
hejia <1464946535@qq.com>
2017-07-25 23:23:25 +0800

    add Joystick

--- Assets/Script/Joystick.cs
+++ Assets/Script/Joystick.cs
@@ -1,9 +1,146 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Joystick
 {
+    private Image mBgImg;
+    private Image mJoystickImg;
+    private Image mThumbImg;
+    private Vector3 mInputVector;
+    private Vector2 mInitPos;
+    private float mBgRadius;
+    private float mJoystickRadius;
+    private float mMoveRadius;
+    private Tweener mTweener;
+    private bool mIsDraging;
 
+    public Joystick(Image bgImg)
+    {
+        mBgImg = bgImg;
+        mJoystickImg = mBgImg.transform.Find("joystick").GetComponent<Image>();
+        mThumbImg = mBgImg.transform.Find("thumb").GetComponent<Image>();
 
+        mThumbImg.gameObject.SetActive(false);
+
+        mInitPos = Vector2.zero;
+
+        mIsDraging = false;
+
+        mBgRadius = mBgImg.rectTransform.sizeDelta.x / 2;
+        mJoystickRadius = mJoystickImg.rectTransform.sizeDelta.x / 2;
+        mMoveRadius = 200;
+
+        Texture2D texture2d = (Texture2D)Resources.Load("none");
+        Rect rect = new Rect(0, 0, texture2d.width, texture2d.height);
+        Sprite sp = Sprite.Create(texture2d, rect, new Vector2(0.5f, 0.5f));//注意居中显示采用0.5f值    
+        mBgImg.sprite = sp;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (mTweener != null)
+        {
+            mTweener.Kill();
+            mTweener = null;
+        }
+
+        mIsDraging = false;
+
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(mBgImg.rectTransform,
+            eventData.position, eventData.pressEventCamera, out pos))
+        {
+            mInitPos = pos;
+
+            if (pos.magnitude + mJoystickRadius < mBgRadius)
+            {
+                mIsDraging = true;
+
+                mJoystickImg.rectTransform.anchoredPosition = new Vector2(pos.x, pos.y);
+
+                mThumbImg.gameObject.SetActive(true);
+
+                float degrees = Mathf.Atan2(pos.y, pos.x) * 180 / Mathf.PI;
+                mThumbImg.rectTransform.eulerAngles = new Vector3(0, 0, degrees - 90);
+                mThumbImg.rectTransform.anchoredPosition = new Vector2(pos.x, pos.y);
+            }
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (!mIsDraging)
+            return;
+
+        Vector2 pos;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(mBgImg.rectTransform,
+            eventData.position, eventData.pressEventCamera, out pos))
+        {
+            Vector2 deltaPos = pos - mInitPos;
+            deltaPos = Vector2.ClampMagnitude(deltaPos, mMoveRadius);
+            pos = deltaPos + mInitPos;
+            mThumbImg.rectTransform.anchoredPosition = new Vector2(pos.x, pos.y);
+
+            float degrees = Mathf.Atan2(deltaPos.y, deltaPos.x) * 180 / Mathf.PI;
+            mThumbImg.rectTransform.eulerAngles = new Vector3(0, 0, degrees - 90);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (!mIsDraging)
+            return;
+        //mInputVector = Vector3.zero;
+        bool _pixelSnapping = true;
+
+        mJoystickImg.gameObject.SetActive(false);
+
+        Vector2 pos = mThumbImg.rectTransform.anchoredPosition;
+        float degrees = Mathf.Atan2(pos.y, pos.x) * 180 / Mathf.PI;
+        mThumbImg.rectTransform.eulerAngles = new Vector3(0, 0, degrees - 90 + 180);
+
+        mTweener = DOTween.To(() => mThumbImg.rectTransform.anchoredPosition, x => mThumbImg.rectTransform.anchoredPosition = x, Vector2.zero, 0.3f)
+                .SetOptions(_pixelSnapping)
+                .SetUpdate(true)
+                .SetTarget(this)
+                .OnComplete(() =>
+                {
+                    mTweener = null;
+
+                    mJoystickImg.gameObject.SetActive(true);
+                    mJoystickImg.rectTransform.anchoredPosition = Vector2.zero;
+
+                    mThumbImg.gameObject.SetActive(false);
+                    mThumbImg.rectTransform.eulerAngles = new Vector3(0, 0, 0);
+                    mThumbImg.rectTransform.anchoredPosition = Vector2.zero;
+                });
+    }
+
+    public float Horizontal()
+    {
+        if (mInputVector.x != 0)
+        {
+            return mInputVector.x;
+        }
+        else
+        {
+            return Input.GetAxis("Horizontal");
+        }
+    }
+
+    public float Vertical()
+    {
+        if (mInputVector.z != 0)
+        {
+            return mInputVector.z;
+        }
+        else
+        {
+            return Input.GetAxis("Vertical");
+        }
+    }
 }
--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -5,21 +5,14 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerUpHandler, IPointerDownHandler
 {
-    private Image mBgImg;
-    private Image mJoystickImg;
-    private Vector3 mInputVector;
+
+    private Joystick mJoystick;
 
     void Start()
     {
-        Text msgBox = transform.Find("MessageBox").GetComponent<Text>();
+        //Text msgBox = transform.Find("MessageBox").GetComponent<Text>();
         Image bgImg = transform.Find("JoystickBg").GetComponent<Image>();
-        Image joystickImg = bgImg.transform.Find("joystick").GetComponent<Image>();
-
-        mBgImg = bgImg;
-        mJoystickImg = joystickImg;
-
-        //bgImg.gameObject.SetActive(false);
-
+        mJoystick = new Joystick(bgImg);
     }
 
     void Update()
@@ -33,59 +26,22 @@ public class UIManager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
     public void OnDrag(PointerEventData eventData)
     {
-        Vector2 pos;
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(mBgImg.rectTransform,
-            eventData.position, eventData.pressEventCamera, out pos))
-        {
-            pos.x = pos.x / mBgImg.rectTransform.sizeDelta.x;
-            pos.y = pos.y / mBgImg.rectTransform.sizeDelta.y;
-
-            //Debug.Log("Hey");
-            mInputVector = new Vector3(pos.x * 2 + 1, 0, pos.y * 2 - 1);
-            mInputVector = (mInputVector.magnitude > 1.0f) ? mInputVector.normalized : mInputVector;
-
-            mJoystickImg.rectTransform.anchoredPosition =
-                new Vector3(mInputVector.x * (mBgImg.rectTransform.sizeDelta.x / 3)
-                , mInputVector.z * (mBgImg.rectTransform.sizeDelta.y / 3));
-        }
+        mJoystick.OnDrag(eventData);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
     }
 
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        OnDrag(eventData);
-    }
-
     public void OnPointerDown(PointerEventData eventData)
     {
-        mInputVector = Vector3.zero;
-        mJoystickImg.rectTransform.anchoredPosition = Vector3.zero;
+        mJoystick.OnPointerDown(eventData);
     }
 
-    public float Horizontal()
+    public void OnPointerUp(PointerEventData eventData)
     {
-        if (mInputVector.x != 0)
-        {
-            return mInputVector.x;
-        }
-        else
-        {
-            return Input.GetAxis("Horizontal");
-        }
+        mJoystick.OnPointerUp(eventData);
+
     }
 
-    public float Vertical()
-    {
-        if (mInputVector.z != 0)
-        {
-            return mInputVector.z;
-        }
-        else
-        {
-            return Input.GetAxis("Vertical");
-        }
-    }
 }
